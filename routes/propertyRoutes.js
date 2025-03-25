abb4dbfa1893bd6262707c1136f57025464887b3
ravindrauiet <ravindranathjha76@gmail.com>
2025-03-25 17:21:06 +0530

    change the routes for the rent and owner property

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,23 +1,24 @@
 // routes/propertyRoutes.js
 import express from 'express';
-import { getPropertyDetails, getAdminProperties,getRentalProperties,getAdminPropertiesByCity } from '../controllers/propertyController.js';
+import { 
+  getPropertyDetails, 
+  getAdminProperties,
+  getRentalProperties,
+  getAdminPropertiesByCity,
+  getRentalPropertiesByCity,       
+  getOwnerPropertiesByCity         
+} from '../controllers/propertyController.js';
 
 const router = express.Router();
 
-// Route to fetch all properties listed by admin
 router.get('/admin', getAdminProperties);
-
-// Route to fetch all properties listed by admin filter by city
 router.get('/adminbycity', getAdminPropertiesByCity);
-
-// Route to fetch all properties which is rental
 router.get('/rentalproperties', getRentalProperties);
+router.get('/ownerproperties', getRentalProperties);  
+// Add new routes for your functions:
+router.get('/rentalpropertiesbycity', getRentalPropertiesByCity);
+router.get('/ownerpropertiesbycity', getOwnerPropertiesByCity);
 
-// Route to fetch all properties listed by owner
-router.get('/ownerproperties', getRentalProperties);
-
-// Route to fetch property details and associated agents/owners
 router.get('/:property_name', getPropertyDetails);
 
 export default router;
-
